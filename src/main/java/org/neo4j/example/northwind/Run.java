aee1f86df5617e044647330f30399c1a2d5d4314
Axel Morgner <axel@morgner.de>
2015-04-14 23:26:06 +0200

    Minor refactoring of runner class

--- src/main/java/org/neo4j/example/northwind/Run.java
+++ src/main/java/org/neo4j/example/northwind/Run.java
@@ -4,7 +4,6 @@ import org.neo4j.example.northwind.model.Category;
 import org.neo4j.example.northwind.model.Customer;
 import org.neo4j.example.northwind.model.Order;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.GenericApplicationContext;
 import org.neo4j.example.northwind.model.Product;
 import org.neo4j.example.northwind.model.Supplier;
 import org.neo4j.example.northwind.repository.CategoryRepository;
@@ -27,16 +26,21 @@ public class Run {
 
 		final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppContext.class);
 
-		listProducts(context);
-		listCustomers(context);
-		listCategories(context);
-		listSuppliers(context);
-		listOrders(context);
+		final ProductRepository  productRepository  = context.getBean(ProductRepository.class);
+		final CustomerRepository customerRepository = context.getBean(CustomerRepository.class);
+		final CategoryRepository categoryRepository = context.getBean(CategoryRepository.class);
+		final SupplierRepository supplierRepository = context.getBean(SupplierRepository.class);
+		final OrderRepository    orderRepository    = context.getBean(OrderRepository.class);
+
+		listProducts(productRepository);
+		listCustomers(customerRepository);
+		listCategories(categoryRepository);
+		listSuppliers(supplierRepository);
+		listOrders(orderRepository);
 
 	}
 
-	public static void listProducts(final GenericApplicationContext context) {
-		final ProductRepository productRepository = context.getBean(ProductRepository.class);
+	public static void listProducts(final ProductRepository  productRepository) {
 		final Iterable<Product> products = productRepository.findAll();
 		for (final Product p : products) {			
 			System.out.println("Product " + p.productName);
@@ -45,8 +49,7 @@ public class Run {
 		}
 	}
 	
-	public static void listCustomers(final GenericApplicationContext context) {
-		final CustomerRepository customerRepository = context.getBean(CustomerRepository.class);
+	public static void listCustomers(final CustomerRepository customerRepository) {
 		final Iterable<Customer> customers = customerRepository.findAll();
 		for (final Customer c : customers) {
 			System.out.println("Customer: " + c.contactName);
@@ -56,8 +59,7 @@ public class Run {
 		}
 	}
 
-	public static void listCategories(final GenericApplicationContext context) {
-		final CategoryRepository categoryRepository = context.getBean(CategoryRepository.class);
+	public static void listCategories(final CategoryRepository categoryRepository) {
 		final Iterable<Category> category = categoryRepository.findAll();
 		for (final Category c : category) {
 			System.out.println("Category: " + c.categoryName);
@@ -67,8 +69,7 @@ public class Run {
 		}
 	}
 
-	public static void listSuppliers(final GenericApplicationContext context) {
-		final SupplierRepository supplierRepository = context.getBean(SupplierRepository.class);
+	public static void listSuppliers(final SupplierRepository supplierRepository) {
 		final Iterable<Supplier> supplier = supplierRepository.findAll();
 		for (final Supplier s : supplier) {
 			System.out.println("Supplier: " + s.companyName);
@@ -78,8 +79,7 @@ public class Run {
 		}
 	}
 	
-	public static void listOrders(final GenericApplicationContext context) {
-		final OrderRepository orderRepository = context.getBean(OrderRepository.class);
+	public static void listOrders(final OrderRepository    orderRepository) {
 		final Iterable<Order> order = orderRepository.findAll();
 		for (final Order o : order) {
 			System.out.println("Order: " + o.orderID);
